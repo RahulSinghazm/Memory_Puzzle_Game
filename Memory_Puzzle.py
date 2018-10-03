aea332a85ee4422b9be2f304b120ae63ddc2507b
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-10-03 23:30:45 +0530

    Update Memory_Puzzle.py

--- Memory_Puzzle.py
+++ Memory_Puzzle.py
@@ -1,7 +1,5 @@
 # Memory Puzzle
-# By Al Sweigart al@inventwithpython.com
-# http://inventwithpython.com/pygame
-# Released under a "Simplified BSD" license
+
 
 import random, pygame, sys
 from pygame.locals import *
